483f70856ecb72fa65f13a0052599bcb0d6970e9
Eric Seokgon Lee <leesk55@gmail.com>
2026-07-17 20:56:38 +0900

    Add files via upload

--- device-api-web/src/main/java/egovframework/hyb/mbl/nwk/service/EgovNetworkAPIService.java
+++ device-api-web/src/main/java/egovframework/hyb/mbl/nwk/service/EgovNetworkAPIService.java
@@ -47,9 +47,9 @@ public interface EgovNetworkAPIService {
     List<?> selectNetworkInfoList(Object vo) throws Exception;
 
     /**
-     * 네트워크 정보 총 갯수를 조회한다.
+     * 네트워크 정보 총 개수를 조회한다.
      * @param vo - 조회할 정보가 담긴 VO
-     * @return 네트워크 정보 총 갯수
+     * @return 네트워크 정보 총 개수
      * @exception
      */
     int selectNetworkInfoListTotCnt(Object vo);
